3bd0a7428c96563d4140087da10eddf6056a080a
winnie20-sam <68070889+winnie20-sam@users.noreply.github.com>
2022-11-07 07:58:24 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date =require(__dirname +  "/date.js");
+
+
+
+const app = express();
+
+const  items = ["buy food","cook food" ,"eat food"];
+const  workItems=[];
+
+app.set("view engine" ,"ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+
+const   day= date.getDate();
+
+res.render("list"  , {listTitle:day , newListItems:items});
+});
+app.post("/" , function(req , res){
+
+  const   item= req.body.newItem;
+
+if(req.body.list==="work"){
+  workItems.push(item);
+  res.redirect("/work");
+}else{
+  items.push(item);
+  res.redirect("/");
+}
+
+});
+
+app.get("/work",  function(req , res){
+  res.render("list" , {listTitle:"work list" , newListItems:workItems});
+});
+
+app.post("/work" , function(req , res){
+
+  const item =req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+});
